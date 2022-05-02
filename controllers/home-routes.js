42dc6d4208900743db88f79ee03800f6e07cc96a
AD <archied5150@yahoo.com>
2022-05-02 12:11:04 -0500

    added handlebars and a front page

---
+++ controllers/home-routes.js
@@ -0,0 +1,46 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models');
+
+//use the same GET route as we did with the api routes to get all posts
+router.get('/', (req, res) => {
+  Post.findAll({
+    attributes: [
+      'id', 
+      'post_url', 
+      'title', 
+      'created_at',
+      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+    ],
+    order: [['created_at', 'DESC']],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          //includes the username  of the commentor
+          model: User,
+          attributes: ['username']
+        }
+      },
+      {
+        //include the username of the poster
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+    .then(dbPostData => {
+      //pass all the post into the homepage
+      //use the .map to create a new array with all data serialized
+      //use the .get to serialize the data, similar to how res.json() does it automatically
+      const posts = dbPostData.map(post => post.get({ plain: true }));
+      res.render('homepage', { posts });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 const express = require('express');
-const routes = require('./routes');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
+
+const hbs = exphbs.create({});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+//set up handlebars engine
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+//express.static is a middleware function that can take contents of a folder and serve them as static assets, useful for front end files
+app.use(express.static(path.join(__dirname, 'public')));
 
 //turn on routes
 app.use(routes);
